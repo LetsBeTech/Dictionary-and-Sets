e7143bdf737d38e3a61415418832714edfbcd2fd
TechnoCodin <86043165+TechnoCodin@users.noreply.github.com>
2021-06-30 13:10:21 +0530

    Dictionary.py

---
+++ Dict_and_Sets.py
@@ -0,0 +1,19 @@
+#Syntax of dictionary
+
+MyDict = {
+"run":"in a quick manner",
+"immortal":"A person who can't die"
+}
+
+#This will return 'in a quick manner'
+print(MyDict["run"])
+
+#This will return "A person who can't die"
+print(MyDict["immortal"])
+
+#prints all the keys present in MyDict
+print(MyDict.keys())
+
+#Prints all the values present in MyDict
+print(MyDict.values())
+
